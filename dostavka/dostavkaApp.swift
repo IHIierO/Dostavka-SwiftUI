4bf12db4f8ae93b76ea9af9530fc76ed3f99f480
Artem Vorobev <t-e-m-a_@mail.ru>
2022-06-06 15:39:31 +0300

    Initial Commit

---
+++ dostavka/dostavkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  dostavkaApp.swift
+//  dostavka
+//
+//  Created by   macbookair132013 on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct dostavkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
